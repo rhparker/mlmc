38115ea5da032cbcf9a91743485f04b8bb5ff752
rhparker <rosshparker@gmail.com>
2015-10-23 13:30:13 -0400

    fixed ctypes/numpy bug for array

--- rbmball.py
+++ rbmball.py
@@ -14,18 +14,21 @@ import ctypes
 _rbmball = ctypes.CDLL('rbmball.so')
 # void mcqmc06_l(int l, int N, int option, double *sums) 
 rbm_ball = _rbmball.rbm_ball
-rbm_ball.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_double * 6) )
+# rbm_ball.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_double * 6) )
+rbm_ball.argtypes = (ctypes.c_int, ctypes.c_int, ndpointer(np.float, flags="C_CONTIGUOUS") )
+
 
 def rbm_ball(l, N):
-    array_type = ctypes.c_double * 6
-    sums = array_type()
+    # array_type = ctypes.c_double * 6
+    # sums = array_type()
+    sums = np.zeros(6)
     _rbmball.rbm_ball(l, N, sums )
-    return sums
+    return sums  
 
 
 # main program
 
-N = 100000  # samples for each level
+N = 10000   # samples for each level
 M  = 2      # refinement cost factor
 N0 = 100    # initial samples on each level
 Lmin = 2    # minimum refinement level
